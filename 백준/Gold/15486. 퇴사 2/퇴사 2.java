c73ca305811594ae481e5aa6aefba3612be5beed
길지운 <90232934+Ji-Un-Gil@users.noreply.github.com>
2023-08-13 21:57:32 +0900

    [Gold V] Title: 퇴사 2, Time: 736 ms, Memory: 315808 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    // static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] T;
+    static int[] P;
+    static int[] dp;
+
+    private static void inputConfig() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        T = new int[N+1];
+        P = new int[N+1];
+        dp = new int[N+2];
+
+        for(int i=1; i<=N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    private static int getMaxPrice() {
+        for(int i = N; i > 0; i--) {
+            int next = i + T[i];
+
+            if(next > N+1) {
+                dp[i] = dp[i+1];
+            } else {
+                dp[i] = Math.max(dp[i+1], P[i] + dp[next]);
+            }
+        }
+        return dp[1];
+    }
+
+    private static void outputTotal(int total) throws IOException {
+        bw.write(String.valueOf(total));
+        bw.flush();
+    }
+
+    public static void main(String[] args) throws IOException {
+        inputConfig();
+        outputTotal(getMaxPrice());
+    }
+}
