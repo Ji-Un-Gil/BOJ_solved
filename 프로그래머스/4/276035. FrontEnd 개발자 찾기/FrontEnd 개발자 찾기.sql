20af216c9176917e19ac9b0fc8769db2954de7a0
길지운 <wldns2577@gmail.com>
2024-11-29 16:50:39 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,5 +1,5 @@
 select id, email, first_name, last_name
-from developers as d
+from developers
 where skill_code & (select code from skillcodes where name = 'JavaScript')
 or skill_code & (select code from skillcodes where name = 'React')
 or skill_code & (select code from skillcodes where name = 'Vue')
